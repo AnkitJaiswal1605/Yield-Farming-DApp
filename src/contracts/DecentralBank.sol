386c6b2612507b4ac09aab7c9ab79f7db1ac1f12
Ankit Jaiswal <ankit.jaiswal1605@gmail.com>
2022-01-26 01:04:39 +0530

    Initial complete commit

---
+++ src/contracts/DecentralBank.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.8.0;
+
+import './RWD.sol';
+import './Tether.sol';
+
+contract DecentralBank {
+  string public name = 'Decentral Bank';
+  address public owner;
+  Tether public tether;
+  RWD public rwd;
+
+  address[] public stakers;
+
+  mapping(address => uint) public stakingBalance;
+  mapping(address => bool) public hasStaked;
+  mapping(address => bool) public isStaked;
+
+  constructor(RWD _rwd, Tether _tether) public {
+      rwd = _rwd;
+      tether = _tether;
+      owner = msg.sender;
+  }
+
+  function depositTokens(uint _amount) public {
+      require(_amount > 0, 'Amount cannot be 0');
+
+      tether.transferFrom(msg.sender, address(this), _amount);
+      stakingBalance[msg.sender] += _amount;
+
+      if(!hasStaked[msg.sender]) {
+          stakers.push(msg.sender);
+      }
+
+      isStaked[msg.sender] = true;
+      hasStaked[msg.sender] = true;
+  }
+
+  function unstakeTokens() public {
+      uint balance = stakingBalance[msg.sender];
+      require(balance > 0, 'no tether staked');
+      tether.transfer(msg.sender, balance);
+
+      stakingBalance[msg.sender] = 0;
+      isStaked[msg.sender] = false;
+  }
+
+  function issueTokens() public {
+      require(msg.sender == owner, 'only owner can issue reward');
+      for(uint i=0; i<stakers.length; i++) {
+          address recipient = stakers[i];
+          uint balance = stakingBalance[recipient] / 9;
+          if(balance>0) {
+              rwd.transfer(recipient, balance);
+          }
+      }
+  }
+  
+}
